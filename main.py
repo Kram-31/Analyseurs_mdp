59126d35381f221e7582d250cb25ed72e0f2ab33
kram-31 <kkaced31@gmail.com>
2025-11-18 16:58:25 +0100

    première verif faite

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
-password = input("Entrez un mot de passe : ")
-if password == "":
-	print("Vous n'avez rien entré !")
+def analyse_len():
+	score_sec = 0
+	len_pass = len(password)
+	print(len_pass)
+	if len_pass >= 20:
+		score_sec += 4 
+	elif len_pass >= 16:
+		score_sec += 3
+	elif len_pass >= 12:
+		score_sec += 2
+	elif len_pass >= 8:
+		score_sec += 1
+	else: 
+		score_sec += 0
+	return score_sec
+def analyser_mot_de_passe():
+	while True:
+		password = input("Entrez un mot de passe : ")
+
+		if password == "":
+			print("\n❌ Erreur : Vous n'avez rien entré ! Veuillez réessayer.")
+			continue
+		else:
+			break
+
+	print(f"\nMot de passe à analyser : **{password}**")	
+	score_sec = 0
+	len_pass = len(password)
+	print(len_pass)
+	if len_pass >= 20:
+		score_sec += 4 
+	elif len_pass >= 16:
+		score_sec += 3
+	elif len_pass >= 12:
+		score_sec += 2
+	elif len_pass >= 8:
+		score_sec += 1
+	else: 
+		score_sec += 0
+	print(f"Votre score est de ",score_sec)
+	if passw
+	
+	return password
+
+if __name__ == "__main__":
+	analyser_mot_de_passe()
